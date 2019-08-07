d5a03e68a7c90325ff0957c062823f00a8c1ea1e
Domesy <397163123@qq.com>
2019-08-07 20:27:04 +0800

    获取CPU占用和内存占用

---
+++ 01-nodeBasics/api/02-useModule.js
@@ -0,0 +1,39 @@
+const os = require('os');
+const util = require('util');
+
+// 安装插件 npm i cpu-stat -s
+const cpuStat = require('cpu-stat');
+
+// os.freemem() 方法以整数的形式回空闲系统内存的字节数。
+// os.totalmem() 方法以整数的形式返回所有系统内存的字节数
+
+// 内存占用 
+const mem = os.freemem() / os.totalmem() * 100;
+
+// toFixed(num) 方法可把 Number 四舍五入为指定小数位数的数字,num位0~20之间的数   
+console.log(`内存占用${mem.toFixed(2)}%`); 
+
+const getCpu = util.promisify(cpuStat.usagePercent);
+
+// 获取电脑cpu占用：
+// cpuStat.usagePercent((err, percent) => {
+//   console.log(`CPU占用:${percent.toFixed(2)}%`);
+// })
+
+// 第二种：用util包装
+getCpu().then(percent => {
+  console.log(`CPU占用:${percent.toFixed(2)}%`);
+});
+
+const showState = async() => {
+  const mem = os.freemem() / os.totalmem() * 100;
+  console.log(`内存占用${mem.toFixed(2)}%`);
+  const percent = await getCpu();
+  console.log(`CPU占用:${percent.toFixed(2)}%`);
+} 
+
+module.exports = {
+  showState
+}
+
+
